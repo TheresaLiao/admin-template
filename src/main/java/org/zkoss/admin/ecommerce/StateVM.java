d6a311e9954f45746b3b3dc0edfb237004c7f544
me <theresahualiao@gmail.com>
2020-04-29 15:01:43 +0800

    add timer

--- src/main/java/org/zkoss/admin/ecommerce/StateVM.java
+++ src/main/java/org/zkoss/admin/ecommerce/StateVM.java
@@ -1,31 +1,46 @@
 package org.zkoss.admin.ecommerce;
 
 import org.zkoss.admin.ecommerce.dao.*;
+import org.zkoss.bind.annotation.Command;
 import org.zkoss.bind.annotation.Init;
+import org.zkoss.bind.annotation.NotifyChange;
+import org.zkoss.zk.ui.select.annotation.Listen;
 
 import java.util.*;
 
 public class StateVM {
-    private List<State> states;
+	private List<State> states;
 
-    @Init
-    public void init(){
-        queryStates();
-    }
+	private int proof = 0;
 
-    private void queryStates() {
-        states = new LinkedList<>();
-        for (int i = 0 ; i < 4 ; i++){
-            State state = new State();
-            state.setType(Type.values()[i % 4]);
-            state.setValue(1317 * (i + 1));
-            state.setRatio(0.329);
-            states.add(state);
-        }
-    }
+	@Init
+	public void init() {
+		queryStates();
+	}
 
-    public List<State> getStates() {
-        return states;
-    }
+	@NotifyChange({"states"})
+	@Command
+	public void refreshInfo() {
+		queryStates();
+	}
+
+	private void queryStates() {
+		proof++;
+		System.out.println(proof);
+		System.out.println("queryStates");
+		states = new LinkedList<>();
+		for (int i = 0; i < 4; i++) {
+			State state = new State();
+			state.setType(Type.values()[i % 4]);
+			state.setValue(proof * (i + 1));
+			state.setRatio(0.329);
+			states.add(state);
+		}
+	}
+
+	public List<State> getStates() {
+		System.out.println("getStates");
+		return states;
+	}
 
 }
